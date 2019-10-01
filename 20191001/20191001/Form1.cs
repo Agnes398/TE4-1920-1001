2dd2d7e1df657a491a38c62c163570e20a11e0fa
Agnes Löjdkvist <agnes.lojdkvist@edu.huddinge.se>
2019-10-01 11:01:23 +0200

    dreated project and GUI layout

---
+++ 20191001/20191001/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20191001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.Text = "Välkommen!";
+            label2.Text = "Vad vill du göra?";
+            button1.Text = "Registrera ny bil";
+            button2.Text = "Visa bilar i lager";
+        }
+    }
+}
