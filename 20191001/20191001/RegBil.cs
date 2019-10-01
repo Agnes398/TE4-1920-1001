1c6eee1a24e67bcd094e6501185951dc78ce6f73
Agnes Löjdkvist <agnes.lojdkvist@edu.huddinge.se>
2019-10-01 11:28:52 +0200

    form for RegBil done

--- 20191001/20191001/RegBil.cs
+++ 20191001/20191001/RegBil.cs
@@ -12,6 +12,7 @@ namespace _20191001
 {
     public partial class RegBil : Form
     {
+        System.Collections.ArrayList MyCars = new System.Collections.ArrayList();
         public RegBil()
         {
             InitializeComponent();
@@ -20,11 +21,23 @@ namespace _20191001
             label2.Text = "Märke";
             label3.Text = "Modell";
             label4.Text = "Kilometer";
+
+            button1.Text = "Registrera";
+            button2.Text = "Klar";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            MyCars.Add(new Bilar() { RegNum = textBox1.Text, Märke = textBox2.Text, Modell = textBox3.Text, Km = textBox4.Text });
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
